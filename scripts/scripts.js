4b5dc3ed9263b65af647a921a788421ee5ed4168
User <zomlesh@yandex.ru>
2021-01-18 18:54:47 +0300

    Добавление новой карточки, реализация лайка карточки

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,18 @@
 let open = document.querySelector('.profile__button-edit'); //кнопка редактирование профиля
-let addCard = document.querySelector('.profile__button-add');
+let addCard = document.querySelector('.profile__button-add'); //кнопка добавления карточки
 let pop = document.querySelector('#profile'); // переменная всплывающего окна профиля
 let popCard = document.querySelector('#cards');// переменная всплвающего окна карточек
 let close = document.querySelectorAll('.popup__close'); // закрывающая окно
-let formElement = document.querySelector('.input'); // элементы в всплывающем окне
+let formElement = document.querySelector('.input_profile'); // элементы в всплывающем окне
+let formCard = document.querySelector('.input_card'); // элементы в всплывающем окне добавления карточки
 let nameInput = document.querySelector('.input__text_text_name'); // селектор для Имени
 let careerInput = document.querySelector('.input__text_text_career');// селектор для работы
 let name = document.querySelector('.profile__heading'); // переменнная Имени в профиле
 let career = document.querySelector('.profile__subheading'); // переменнная работы в профиле
+let namePlace = document.querySelector('.input__text_text_element') //селектор название места
+let linkPlace = document.querySelector('.input__text_text_link') //селектор ссылка на фотографию
 let cardsSection = document.querySelector('.elements');// переменная карточки
+
 //массив с дефолтными карточками
 const initialCards = [
   {
@@ -38,50 +42,51 @@ const initialCards = [
 ]; 
 
 //наполнение дефолтными карточками
+const elementTemplate = document.querySelector('#element').content;
 initialCards.forEach(function(item) {
-  const elementTemplate = document.querySelector('#element').content;
-  const cardElement = elementTemplate.cloneNode(true);
+    const cardElement = elementTemplate.cloneNode(true);
     cardElement.querySelector('.element__image').style.backgroundImage = 'url' + "('" + item['link'] + "')";
     cardElement.querySelector('.element__heading').textContent = item['name'];
     cardsSection.append(cardElement);
 });
 
-// function openPopup() {
-//   nameInput.value = name.textContent;
-//   careerInput.value = career.textContent;
-//   pop.classList.add('popup_opened');
-// }
-
 function closePopup() {
   pop.classList.remove('popup_opened');
   popCard.classList.remove('popup_opened');
-}
+};
 
 
-function handleFormSubmit(evt) {
-    evt.preventDefault();
+formElement.addEventListener('submit', function(evt){
+      evt.preventDefault();
       name.textContent = nameInput.value;
       career.textContent = careerInput.value;
       closePopup();
-}
+});
 
-// open.addEventListener('click', openPopup);
-// close.addEventListener('click', closePopup);
-// addCard.addEventListener('click', openPopup);
-formElement.addEventListener('submit', handleFormSubmit);
+formCard.addEventListener('submit', function(evt){
+  evt.preventDefault();
+  const newCardElement = elementTemplate.cloneNode(true);
+  newCardElement.querySelector('.element__heading').textContent = namePlace.value;
+  newCardElement.querySelector('.element__image').style.backgroundImage = 'url' + "('" + linkPlace.value + "')";
+  cardsSection.prepend(newCardElement);
+  closePopup();
+});
 
-// close.addEventListener('click', function() {
-//   popCard.classList.remove('popup_opened');
-//   pop.classList.remove('popup_opened');
-// });
+open.addEventListener('click', function() {
+  nameInput.value = name.textContent;
+  careerInput.value = career.textContent;
+  pop.classList.add('popup_opened');
+});
 
-  open.addEventListener('click', function() {
-    nameInput.value = name.textContent;
-    careerInput.value = career.textContent;
-    pop.classList.add('popup_opened');
-  });
+addCard.addEventListener('click', function() { 
+  namePlace.value = namePlace.getAttribute('placeholder');
+  linkPlace.value = linkPlace.getAttribute('placeholder');
+  popCard.classList.add('popup_opened');
+});
 
-  addCard.addEventListener('click', function() {
-    popCard.classList.add('popup_opened');
-    });
-
+let likeElement = document.querySelectorAll('.element__like');//переменная лайк места
+likeElement.forEach(function(item){
+  item.addEventListener('click', function(){
+    item.classList.toggle('element__like_black');
+  })
+})
