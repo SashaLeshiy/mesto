443ba8946792a1d02afff3df85d1405d1be2b0a3
User <zomlesh@yandex.ru>
2021-01-19 19:24:33 +0300

    Реализация корзины

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,3 @@
-let open = document.querySelector('.profile__button-edit'); //кнопка редактирование профиля
-let addCard = document.querySelector('.profile__button-add'); //кнопка добавления карточки
-let pop = document.querySelector('#profile'); // переменная всплывающего окна профиля
-let popCard = document.querySelector('#cards');// переменная всплвающего окна карточек
-let close = document.querySelectorAll('.popup__close'); // закрывающая окно
-let formElement = document.querySelector('.input_profile'); // элементы в всплывающем окне
-let formCard = document.querySelector('.input_card'); // элементы в всплывающем окне добавления карточки
-let nameInput = document.querySelector('.input__text_text_name'); // селектор для Имени
-let careerInput = document.querySelector('.input__text_text_career');// селектор для работы
-let name = document.querySelector('.profile__heading'); // переменнная Имени в профиле
-let career = document.querySelector('.profile__subheading'); // переменнная работы в профиле
-let namePlace = document.querySelector('.input__text_text_element') //селектор название места
-let linkPlace = document.querySelector('.input__text_text_link') //селектор ссылка на фотографию
-let cardsSection = document.querySelector('.elements');// переменная карточки
-
 //массив с дефолтными карточками
 const initialCards = [
   {
@@ -41,11 +26,28 @@ const initialCards = [
   }
 ]; 
 
+let open = document.querySelector('.profile__button-edit'); //кнопка редактирование профиля
+let addCard = document.querySelector('.profile__button-add'); //кнопка добавления карточки
+let pop = document.querySelector('#profile'); // переменная всплывающего окна профиля
+let popCard = document.querySelector('#cards');// переменная всплвающего окна карточек
+let openImg = document.querySelector('#bigImage');// переменная всплвающего окна картинки
+let close = document.querySelectorAll('.popup__close'); // закрывающая окно
+let formElement = document.querySelector('.input_profile'); // элементы в всплывающем окне
+let formCard = document.querySelector('.input_card'); // элементы в всплывающем окне добавления карточки
+let nameInput = document.querySelector('.input__text_text_name'); // селектор для Имени
+let careerInput = document.querySelector('.input__text_text_career');// селектор для работы
+let name = document.querySelector('.profile__heading'); // переменнная Имени в профиле
+let career = document.querySelector('.profile__subheading'); // переменнная работы в профиле
+let namePlace = document.querySelector('.input__text_text_element') //селектор название места
+let linkPlace = document.querySelector('.input__text_text_link') //селектор ссылка на фотографию
+let cardsSection = document.querySelector('.elements');// переменная карточки
+
 //наполнение дефолтными карточками
 const elementTemplate = document.querySelector('#element').content;
 initialCards.forEach(function(item) {
     const cardElement = elementTemplate.cloneNode(true);
-    cardElement.querySelector('.element__image').style.backgroundImage = 'url' + "('" + item['link'] + "')";
+    cardElement.querySelector('.element__image').src = item['link'];
+    cardElement.querySelector('.element__image').alt = `Фотография ${item['name']}`;
     cardElement.querySelector('.element__heading').textContent = item['name'];
     cardsSection.append(cardElement);
 });
@@ -54,7 +56,6 @@ function closePopup() {
   pop.classList.remove('popup_opened');
   popCard.classList.remove('popup_opened');
 };
-
 
 formElement.addEventListener('submit', function(evt){
       evt.preventDefault();
@@ -67,7 +68,8 @@ formCard.addEventListener('submit', function(evt){
   evt.preventDefault();
   const newCardElement = elementTemplate.cloneNode(true);
   newCardElement.querySelector('.element__heading').textContent = namePlace.value;
-  newCardElement.querySelector('.element__image').style.backgroundImage = 'url' + "('" + linkPlace.value + "')";
+  newCardElement.querySelector('.element__image').src = linkPlace.value;
+  newCardElement.querySelector('.element__image').setAttribute('alt', `Фотография ${namePlace.value}`);
   cardsSection.prepend(newCardElement);
   closePopup();
 });
@@ -84,9 +86,19 @@ addCard.addEventListener('click', function() {
   popCard.classList.add('popup_opened');
 });
 
+
+
 let likeElement = document.querySelectorAll('.element__like');//переменная лайк места
 likeElement.forEach(function(item){
   item.addEventListener('click', function(){
     item.classList.toggle('element__like_black');
   })
 })
+
+let deleteElement = document.querySelectorAll('.element__trash');
+deleteElement.forEach(function(item) {
+  item.addEventListener('click', function(){
+    item.closest('.element').remove();
+  })
+})
+
