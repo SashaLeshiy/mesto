5f6e509e5a15af229e19bcd00ed97767e9eb6cc8
User <zomlesh@yandex.ru>
2021-01-22 12:13:26 +0300

    Правка в условии при клике в карточке

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -117,7 +117,7 @@ function eventObj(evt) {
     openImg.classList.add('popup_opened')}
     else if(evt.target.className === ('element__trash')){//если клик на корзину удаляет карточку
         evt.target.closest('.element').remove();
-      } else if(evt.target.classList.contains('element__like')){//ставит/снимает лайк
+      } else {//ставит/снимает лайк
         evt.target.classList.toggle('element__like_black');
       } 
       }
